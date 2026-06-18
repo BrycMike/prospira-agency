310d691b7f228f12f983cd69201a44ba94dc58e1
Sony Sony <son431432@gmail.com>
2026-06-18 14:19:46 +0000

    update the image on the hero section to this  image on this link https:/

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.phototourl.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
